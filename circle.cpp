cb85cd4e381537060813c6cbb6475f43cc56ebc0
ch zulqarnain goraya <zulqarzulqar07@gmail.com>
2022-12-14 11:03:57 -0800

    messi

---
+++ circle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "                **            " << endl;
+cout << "             **    **         " << endl;
+cout << "          **          **      " << endl;
+cout << "          **          **      " << endl;
+cout << "          **          **      " << endl;
+cout << "          **          **      " << endl;
+cout << "             **    **         " << endl;
+cout << "                **            " << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "           *           " << endl;
+cout << "         *   *         " << endl;
+cout << "       *       *       " << endl;
+cout << "     *           *     " << endl;
+cout << "   * * * *  *  *  *   " << endl;
+    
+
+
+
+
+}
