e4a9e4d3497f869f7afd05ccd3f433de0a3e8f84
ch zulqarnain goraya <zulqarzulqar07@gmail.com>
2022-12-17 09:11:06 -0800

    pd week2

---
+++ New folder/color.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main() {
+system("color 07");
+cout <<"******" <<endl;
+system("color o4");
+cout <<" ABC  " <<endl;
+system("color 07");
+cout <<"******" <<endl;
+}
---
+++ New folder/cow.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main(){
+
+cout <<"         -----------------------      " <<endl;
+cout <<"       o    ^__^                      " <<endl;
+cout <<"        o   (oo)\\___________          " <<endl;
+cout <<"            (__)\\           )\\/\\      " <<endl;
+cout <<"                ||--------w |         " <<endl;
+cout <<"                ||         ||         " <<endl;
+}
